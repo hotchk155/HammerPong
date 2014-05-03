4bcebbdc68bc7654229ace34489da1363794a259
Jason Hotchkiss <jason_hotchkiss@hotmail.com>
2014-05-03 23:31:15 +0100

    input listener

--- InputListener/InputListener.c
+++ InputListener/InputListener.c
@@ -1,95 +1,210 @@
 #include <system.h>
 #include <memory.h>
 
-#define ANA_0 0b00000000
-#define ANA_1 0b00000100
-#define ANA_2 0b00001000
-#define ANA_3 0b00001100
-#define ANA_4 0b00010000
-#define ANA_5 0b00010100
-#define ANA_6 0b00011000
-#define ANA_7 0b00011100
-#define ADC_MAX	6
 
-#define P_HEARTBEAT 	porta.5
+#pragma DATA _CONFIG1, _FOSC_INTOSC & _WDTE_OFF & _MCLRE_OFF &_CLKOUTEN_OFF
+#pragma DATA _CONFIG2, _WRT_OFF & _PLLEN_OFF & _STVREN_ON & _BORV_19 & _LVP_OFF
+#pragma CLOCK_FREQ 16000000
 
-#pragma DATA _CONFIG, _MCLRE_OFF&_WDT_OFF&_INTRC_OSC_NOCLKOUT
-#pragma CLOCK_FREQ 8000000
 #define ADC_AQUISITION_DELAY 10
+
+#define TIMER_2_INIT_SCALAR		5	// Timer 2 is an 8 bit timer counting at 250kHz
+									// using this init scalar means that rollover
+									// interrupt fires once per ms
+
+#define P_TRIG 		portc.1
+#define P_HEARTBEAT 		portc.2
+#define NUM_INPUT_PORTS 	1
+#define TRIGGER_THRESHOLD 	200		// lowest ADC level that is considered a trigger
+#define INPUT_SAMPLE_TIME	30			// milliseconds
+#define INPUT_DEAD_TIME		300			// milliseconds
 
 typedef unsigned char byte;
 
+// ADC states
 enum {
 	ADC_CONNECT,
 	ADC_ACQUIRE,
 	ADC_CONVERT				
 };
 
+// Enumerate the control bit combinations for
+// reading each analog input port 
+enum {
+	ANA_0=(byte)0b00000000,
+	ANA_1=(byte)0b00000100,
+	ANA_2=(byte)0b00001000,
+	ANA_3=(byte)0b00001100,
+	ANA_4=(byte)0b00010000,
+	ANA_5=(byte)0b00010100,
+	ANA_6=(byte)0b00011000,
+	ANA_7=(byte)0b00011100
+};
+
+////////////////////////////////////////////////////////////
+// INPUT TRIGGER STRUCTURE
+typedef struct 
+{
+	byte adcInputMask;		// PIC analog input port mask
+	byte midiChannel;		// MIDI channel on which to notify this trigger
+	byte midiNote;			// MIDI note on which to notify this trigger
+	int triggerThreshold;	// Lowest valid ADC level
+	int maxLevel;			// Highest valid ADC level
+	int thisMaxInput;
+	unsigned long endOfSampleTime;	// End of sample time period
+	unsigned long endOfDeadTime;	// End of dead time period
+} INPUT_PORT;
+
+
+INPUT_PORT inputPort[NUM_INPUT_PORTS];		// input port data
+byte whichInputPort = 0;					// iterator of input ports
+int adcState = ADC_CONNECT;					// master adc state machine state
+volatile unsigned long milliseconds = 0;	// milliseconds counter
+volatile byte millisecondsRollover = 0;		// flag to say if milliseconds have rolled over
+
+
+////////////////////////////////////////////////////////////
+// INTERRUPT HANDLER 
+// Used to maintain a millisecond clock
+void interrupt( void )
+{
+	// timer 0 rollover ISR. Counts ms ticks
+	if(intcon.2)
+	{
+		tmr0 = TIMER_2_INIT_SCALAR;
+		++milliseconds;
+		if(!++milliseconds)
+			millisecondsRollover = 1; // make sure we know if the ms counter overflows
+		intcon.2 = 0;
+	}
+}
+
+////////////////////////////////////////////////////////////
+// INIT SERIAL PORT FOR MIDI XMIT
 void init_usart()
 {
-	pir1.1 = 1;	//TXIF transmit enable
-	pie1.1 = 0;	//TXIE no interrupts
+	pir1.1 = 0;		//TXIF 		
+	pir1.5 = 0;		//RCIF
 	
-	baudctl.4 = 0;		// synchronous bit polarity 
-	baudctl.3 = 1;		// enable 16 bit brg
-	baudctl.1 = 0;		// wake up enable off
-	baudctl.0 = 0;		// disable auto baud detect
+	pie1.1 = 0;		//TXIE 		no interrupts
+	pie1.5 = 0;		//RCIE 		enable
+	
+	baudcon.4 = 0;	// SCKP		synchronous bit polarity 
+	baudcon.3 = 1;	// BRG16	enable 16 bit brg
+	baudcon.1 = 0;	// WUE		wake up enable off
+	baudcon.0 = 0;	// ABDEN	auto baud detect
 		
-	txsta.6 = 0;	// 8 bit transmission
-	txsta.5 = 1;	// transmit enable
-	txsta.4 = 0;	// async mode
-	txsta.2 = 0;	// high baudrate BRGH
+	txsta.6 = 0;	// TX9		8 bit transmission
+	txsta.5 = 1;	// TXEN		transmit enable
+	txsta.4 = 0;	// SYNC		async mode
+	txsta.3 = 0;	// SEDNB	break character
+	txsta.2 = 0;	// BRGH		high baudrate 
+	txsta.0 = 0;	// TX9D		bit 9
 
-	rcsta.7 = 1;	// serial port enable
-	rcsta.6 = 0;	// 8 bit operation
-	rcsta.4 = 0;	// enable receiver
+	rcsta.7 = 1;	// SPEN 	serial port enable
+	rcsta.6 = 0;	// RX9 		8 bit operation
+	rcsta.5 = 0;	// SREN 	enable receiver
+	rcsta.4 = 0;	// CREN 	continuous receive enable
 		
 	spbrgh = 0;		// brg high byte
-	spbrg = 15;		// brg low byte (31250)	
+	spbrg = 31;		// brg low byte (31250)	
 }
 		
+////////////////////////////////////////////////////////////
+// FUNCTION TO SEND A CHARACTER TO UART
 void send(unsigned char c)
 {
 	txreg = c;
 	while(!txsta.1);
 }
 
-void sendController(byte channel, byte controller, byte value)
+////////////////////////////////////////////////////////////
+// SEND MIDI NOTE MESSAGE
+void midiNote(byte chan, byte note, byte vel)
 {
-	P_HEARTBEAT = 1;
-	send(0xb0 | channel);
-	send(controller&0x7f);
-	send(value&0x7f);
-	P_HEARTBEAT = 0;	
+	send(0x90|(chan&0x0F));
+	send(note&0x7f);
+	send(vel&0x7f);
 }
 
-byte adcInput[ADC_MAX] = {ANA_2, ANA_3, ANA_4, ANA_5, ANA_6, ANA_7};
-byte adcInitComplete = 0;
-int adcResult[ADC_MAX] = {-1,-1,-1,-1,-1,-1};
-int adcIndex = 0;
-int adcState = ADC_CONNECT;
+////////////////////////////////////////////////////////////
+// INITIALISE PORT DATA
+void initInput(INPUT_PORT *pInput, byte inputMask, byte chan, byte note)
+{
+	pInput->adcInputMask = inputMask;
+	pInput->midiChannel = chan;
+	pInput->midiNote = note;
+	pInput->triggerThreshold = TRIGGER_THRESHOLD;
+	pInput->maxLevel = 1023;
+	pInput->endOfDeadTime = 0;
+	pInput->endOfSampleTime = 0;
+}
 
-////////////////////////////////////////////////////////////////
-// 
-// doADC
-//
-// State machine for running the ADC and updating the adcResult
-// array with the result from each analog input
-//
-void doADC()
+////////////////////////////////////////////////////////////
+// INPUT PORT ADC DATA HANDLER
+void handleAdcResult(INPUT_PORT *pInput, int adcResult)
+{
+
+	// are we waiting following previous trigger?
+	if(pInput->endOfSampleTime) 
+	{
+		if(adcResult > pInput->thisMaxInput)
+			pInput->thisMaxInput = adcResult;
+		if(milliseconds >= pInput->endOfSampleTime)		
+		{
+			// calculate the MIDI velocity for the hit
+			long velocity = 127 * (pInput->thisMaxInput - pInput->triggerThreshold);
+			velocity /= (pInput->maxLevel - pInput->triggerThreshold);
+			if(velocity > 127)
+				velocity = 127;
+			if(velocity > 0)
+			{
+				// play the MIDI note
+				midiNote(pInput->midiChannel, pInput->midiNote, (byte)velocity);
+				P_TRIG = 1;
+			
+			}			
+			
+			// set up dead time			
+			pInput->endOfSampleTime = 0;
+			pInput->endOfDeadTime = milliseconds + INPUT_DEAD_TIME;
+		}		
+	}
+	else if(pInput->endOfDeadTime) 
+	{
+		// if we have waited long enough then clear the dead time
+		if(milliseconds > pInput->endOfDeadTime)
+		{
+			midiNote(pInput->midiChannel, pInput->midiNote, 0);
+			pInput->endOfDeadTime = 0;
+			P_TRIG = 0;
+		}
+	}
+	// otherwise see if the input is above the trigger threshold
+	else if(adcResult >= pInput->triggerThreshold)
+	{	
+		pInput->endOfSampleTime = milliseconds + INPUT_SAMPLE_TIME;
+		pInput->endOfDeadTime = 0;
+		pInput->thisMaxInput = adcResult;		
+	}	
+}
+
+////////////////////////////////////////////////////////////
+// RUN THE INPUT STATE MACHINE
+void runInputs()
 {
 	switch(adcState)
 	{
-		// Connect ADC to the correct analog input
+		// Connect ADC to the next analog input
 		case ADC_CONNECT:			
-			adcon0=0b10000001 | adcInput[adcIndex];
-			tmr0 = 0;
+			adcon0=0b00000001 | inputPort[whichInputPort].adcInputMask;
+			tmr2 = 0;
 			adcState = ADC_ACQUIRE;
-			// fall through
+			break;
 			
 		// Waiting for a delay while the ADC input settles
 		case ADC_ACQUIRE:
-			if(tmr0 > ADC_AQUISITION_DELAY)
+			if(tmr2 > ADC_AQUISITION_DELAY) // allowing settling time
 			{
 				// Start the conversion
 				adcon0.1=1;
@@ -99,17 +214,14 @@ void doADC()
 			
 		// Waiting for the conversion to complete
 		case ADC_CONVERT:
-			if(!adcon0.1)
+			if(!adcon0.1) // Conversion complete
 			{
-				// store the result
-				adcResult[adcIndex] = (((int)adresh)<<8)|adresl;				
+				// notify the result				
+				handleAdcResult(&inputPort[whichInputPort], (((int)adresh)<<8)|adresl);				
 				
-				// and prepare for the next ADC
-				if(++adcIndex>=ADC_MAX)
-				{
-					adcIndex = 0;
-					adcInitComplete = 1;
-				}
+				// and prepare for the next port
+				if(++whichInputPort>=NUM_INPUT_PORTS)
+					whichInputPort=0;
 				adcState = ADC_CONNECT;				
 			}
 			break;
@@ -117,112 +229,75 @@ void doADC()
 }
 
 
-#define BUFLEN	8
-typedef struct 	
-{
-  char midiChannel;
-  char midiController;  
-  int minADC;
-  int maxADC;
-  char currentValue;  
-  char history[BUFLEN];
-} CONTROLLER;
-
-CONTROLLER controllers[ADC_MAX] = {0};
-
-void initInput(int which, byte channel, byte controller)
-{
-	controllers[which].midiChannel = channel;
-	controllers[which].midiController = controller;
-	controllers[which].minADC = -1;
-	controllers[which].maxADC = -1;
-	controllers[which].currentValue = -1;
-}
-
-void checkInput(int which)
-{
-   // pointer to the controllers
-  CONTROLLER *p = &controllers[which];
-  
-  // read the raw analog value 0-1023
-  int adc = adcResult[which];
-
-  // remember highest and lowest values
-  if((p->minADC == -1) || (p->minADC > adc))
-      p->minADC = adc;
-  if((p->maxADC == -1) || (p->maxADC < adc))
-      p->maxADC = adc;
-         
-  // get the range of known readings
-  int range = p->maxADC - p->minADC;     
-  if(range < 1)
-     range = 1;
-         
-  // scale the current value into the range
-  // NB no floating point support...
-  int newValue = (127*(adc - p->minADC))/range;
-              
-  // add the value into the history buffer
-  long smoothed = 0;
-  for(int j=0; j<BUFLEN-1;++j)
-  {
-    p->history[j] = p->history[j+1];
-    smoothed += p->history[j];
-  }
-  p->history[BUFLEN-1] = newValue;
-  smoothed += newValue;
-  smoothed /= BUFLEN;
-              
-  // has the value changed?
-  if(smoothed != p->currentValue)
-  {
-	 sendController(p->midiChannel, p->midiController, smoothed);
-     p->currentValue= smoothed;
-  }
-}
-
 void main()
 { 
 	int i;
 	
-	// osc control / 8MHz / internal
-	osccon = 0b01110001;
-	
-	// timer0... configure source and prescaler
-	option_reg = 0b10000011;
-	cmcon0 = 7;                      
+	// osc control / 16MHz / internal
+	osccon = 0b01111010;
 	
 	// configure io
-	trisa = 0b00001010;              	
-    trisc = 0b00001111;              
-	ansel = 0b11111100;
-      
+	//apfcon0.7=0;  // usart rx on rc5
+	//apfcon0.2=0;  // usart tx on rc4
+	trisa =  0b00010111;              	
+    trisc =  0b00111000;              
+	ansela = 0b00010111;
+	anselc = 0b00000000;
+	porta=0;
+	portc=0;
+
+	
+	// Configure timer 0 (controls millisecond counter)
+	// 	timer 0 runs at 4MHz
+	// 	prescaled 1/16 = 250kHz
+	// 	rollover at 250 = 1kHz
+	// 	1ms per rollover	
+	option_reg.5 = 0; // timer 0 driven from instruction cycle clock
+	option_reg.3 = 0; // timer 0 is prescaled
+	option_reg.2 = 0; // }
+	option_reg.1 = 1; // } 1/16 prescaler
+	option_reg.0 = 1; // }
+	intcon.5 = 1; 	  // enabled timer 0 interrrupt
+	intcon.2 = 0;     // clear interrupt fired flag
+	intcon.7 = 1;	  // GIE	
+	intcon.6 = 1;	  // PEIE
+	
+	// Configure timer 2 (used to time ADC acquisition)
+	t2con = 0b00000110;
+	      	      
 	// turn on the ADC
-	adcon1=0b00100000; //fOSC/32
-	adcon0=0b10000001; // Right justify / Vdd / AD on
+	adcon0=0b00000001; //AD on
+	adcon1=0b10100000; //right justify, fosc/32, Vss
 	
 	// initialise MIDI comms
 	init_usart();
-	
-	// Initialise the controllers
-	initInput(0, 0, 1);
-	initInput(1, 0, 2);
-	initInput(2, 0, 4);
-	initInput(3, 0, 7);
-	initInput(4, 0, 11);
-	initInput(5, 0, 74);
 
-	adcInitComplete = 0;
+	initInput(&inputPort[0], ANA_3, 0, 60);
+	adcState = ADC_CONNECT;
+	whichInputPort = 0;					
+
+	P_TRIG=0;
+	
+	// main loop
 	for(;;)
 	{
-		doADC();
-		if(adcInitComplete)
+		P_HEARTBEAT = !!(milliseconds & 0x100);
+
+		if(millisecondsRollover)
 		{			
-			for(i=0;i<ADC_MAX;++i)
-				checkInput(i);
-			adcInitComplete = 0;			
-			delay_ms(20);
+			// ensure that if there is a roll over of the millisecond
+			// counter then inputs cannot get jammed into their dead
+			// time
+			for(int i = 0; i<NUM_INPUT_PORTS; ++i)
+			{
+				inputPort[i].endOfSampleTime = 0;
+				inputPort[i].endOfDeadTime = 0;
+			}
+			millisecondsRollover = 0;
 		}
+		
+		// process ADC inputs
+		runInputs();
 	}
 }
 
