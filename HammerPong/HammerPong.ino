72ce20c8841d703a38447c23250063b8675f5658
Jason Hotchkiss <jason_hotchkiss@hotmail.com>
2014-06-11 23:02:23 +0100

    updates

--- HammerPong/HammerPong.ino
+++ HammerPong/HammerPong.ino
@@ -1,32 +1,45 @@
+/*
+  Don't rely on millis... gets slowed when interrupts are turned off during strip updates
+*/
 #include <Arduino.h>
 
-#define PIX_LENGTH 150
-#define PIX_BUFSSIZE (3 * PIX_LENGTH)
-#define PIX_NUMBUFFERS 3
-byte pixBuffer[PIX_NUMBUFFERS][PIX_BUFSSIZE];
+#define PIX_LENGTH 150                        // Number of pixels in a strip
+#define PIX_BUFSSIZE (3 * PIX_LENGTH)         // Number of bytes per strip (24 bit RGB)
+#define PIX_NUMBUFFERS 6                      // Total number of strips in the application
 
+// Define the data pins for the strips. They must all be on the same GPIO port
 #define P_STRIP0 5
 #define P_STRIP1 6
 #define P_STRIP2 7
 #define P_STRIP3 36
 #define P_STRIP4 37
 #define P_STRIP5 38
+
+// Pin for the hearbeat LED
 #define P_HEARTBEAT 13
 
+// Timing constants
 #define SCALE      VARIANT_MCK / 2UL / 1000000UL
 #define INST       (2UL * F_CPU / VARIANT_MCK)
-#define TIME_800_0 ((int)(0.40 * SCALE + 0.5) - (5 * INST))
-#define TIME_800_1 ((int)(0.80 * SCALE + 0.5) - (5 * INST))
-#define PERIOD_800 ((int)(1.25 * SCALE + 0.5) - (5 * INST))
+#define TICKS_PHASE_0 ((int)(0.40 * SCALE + 0.5) - (5 * INST))
+#define TICKS_PHASE_1 ((int)(0.80 * SCALE + 0.5) - (5 * INST))
+#define TICKS_PHASE_2 ((int)(1.25 * SCALE + 0.5) - (5 * INST))
 
+byte pixBuffer[PIX_NUMBUFFERS][PIX_BUFSSIZE]; // The huge display buffer!
+uint32_t endTime;  // Instant in time the strips were last refreshed, used to enfore minimum inter-refresh delay
 
-uint32_t endTime;
+/////////////////////////////////////////////////////////////////////
+// CLEAR DISPLAY BUFFER
 void pixClear()
 {
   memset(pixBuffer, 0, PIX_NUMBUFFERS * PIX_BUFSSIZE);
 }
+
+/////////////////////////////////////////////////////////////////////
+// ONE OFF INITIALISATION OF THE DISPLAY DRIVE CODE
 void pixInit()
 {
+  // Set up pins
   pinMode(P_STRIP0, OUTPUT);
   pinMode(P_STRIP1, OUTPUT);
   pinMode(P_STRIP2, OUTPUT);
@@ -34,48 +47,52 @@ void pixInit()
   pinMode(P_STRIP4, OUTPUT);
   pinMode(P_STRIP5, OUTPUT);
 
-  pinMode(P_HEARTBEAT, OUTPUT);
-  
+  // Set default output value
   digitalWrite(P_STRIP0, LOW);
   digitalWrite(P_STRIP1, LOW);
   digitalWrite(P_STRIP2, LOW);
   digitalWrite(P_STRIP3, LOW);
   digitalWrite(P_STRIP4, LOW);
   digitalWrite(P_STRIP5, LOW);
-  digitalWrite(P_HEARTBEAT, LOW);
-  
-  endTime = 0;
-  pixClear();
 
+  // Clear the display  
+  pixClear();
+  
+  // Initialise the "last update time"
+  endTime = 0;
 }
+
+/////////////////////////////////////////////////////////////////////
+// REFRESH ALL 6 LED STRIPS
+// Based on Adafruit Neopixel code
+// Modified to drive six strips in parallel
 void pixUpdate()
 {
-
+  // Ensure that the minimum "latch" command time has elapsed since the 
+  // last refresh
   while ((micros() - endTime) < 50L);
-  noInterrupts(); // Need 100% focus on instruction timing
-
-
-
+  
+  // turn off interrupts
+  noInterrupts(); 
+  
+  // Get the timer ready
   pmc_set_writeprotect(false);
   pmc_enable_periph_clk((uint32_t)TC3_IRQn);
   TC_Configure(TC1, 0, TC_CMR_WAVE | TC_CMR_WAVSEL_UP | TC_CMR_TCCLKS_TIMER_CLOCK1);
   TC_Start(TC1, 0);
-int t0 = TIME_800_0;
-int t1 = TIME_800_1;
-int period = PERIOD_800;
-
-  // get port address...
-  // Note: all the pins need to be on same port reg
+  
+  // get port address... Note: all the pins need to be on same port reg
   Pio *port      = g_APinDescription[P_STRIP0].pPort; 
   
   // Get register addresses
   volatile WoReg *portSet   = &(port->PIO_SODR);            
   volatile WoReg *portClear = &(port->PIO_CODR);            
+  
+  // Get timer
   volatile WoReg *timeValue = &(TC1->TC_CHANNEL[0].TC_CV);  
   volatile WoReg *timeReset = &(TC1->TC_CHANNEL[0].TC_CCR);
 
-
-  int index = 0;
+  // Get the pin mask for each pin driving a strip
   int pinMask0 = g_APinDescription[P_STRIP0].ulPin; 
   int pinMask1 = g_APinDescription[P_STRIP1].ulPin; 
   int pinMask2 = g_APinDescription[P_STRIP2].ulPin; 
@@ -83,8 +100,16 @@ int period = PERIOD_800;
   int pinMask4 = g_APinDescription[P_STRIP4].ulPin; 
   int pinMask5 = g_APinDescription[P_STRIP5].ulPin; 
 
+  // The strip is driven in 3 phases... 
+  // Phase 0: All the output pins are HIGH
+  // Phase 1: All those output pins transmitting a "zero" are set low
+  // Phase 2: All the remaining pins are set low
+  // 
+  // Set up the bit mask for phase 0...
   int phase0 = pinMask0|pinMask1|pinMask2|pinMask3|pinMask4|pinMask5;
-  while(index < PIX_BUFSSIZE)  
+  
+  // Loop through all the bytes per strip
+  for(int index = 0; index < PIX_BUFSSIZE; ++index)  
   {          
     // Fetch all six colour byte values
     byte colourValue0 = pixBuffer[0][index];
@@ -94,102 +119,145 @@ int period = PERIOD_800;
     byte colourValue4 = pixBuffer[4][index];
     byte colourValue5 = pixBuffer[5][index];
 
+    // Loop through the bits per byte
     uint8_t bitMask = 0x80;
     while(bitMask)
     {    
-      int phase1 = phase0; // 111111
+      // Calculate the phase 1 value (bit mask of all 
+      // those bits corresponding to the output pins
+      // for strips to which we're sending a "zero" bit
+      int phase1 = phase0; 
       if(colourValue0 & bitMask) phase1 ^= pinMask0; 
       if(colourValue1 & bitMask) phase1 ^= pinMask1; 
       if(colourValue2 & bitMask) phase1 ^= pinMask2; 
       if(colourValue3 & bitMask) phase1 ^= pinMask3; 
       if(colourValue4 & bitMask) phase1 ^= pinMask4; 
       if(colourValue5 & bitMask) phase1 ^= pinMask5; 
+      
+      // Calculate phase 2 value (bit mask of all those
+      // output pins that were not cleared at phase 1)
       int phase2 = phase0 ^ phase1; 
       
-      while (*timeValue < period);    
+      // Wait until we're ready to start the next bit
+      while (*timeValue < TICKS_PHASE_2);    
       
-      // set all outputs high
+      // Phase 0: All outputs high, reset the timer
+      // and wait for the phase 0 duration
       *portSet   = phase0;
-      *timeReset = TC_CCR_CLKEN | TC_CCR_SWTRG;
-      while (*timeValue < t0);
+      *timeReset = TC_CCR_CLKEN | TC_CCR_SWTRG;      
+      while (*timeValue < TICKS_PHASE_0);
+      
+      // Phase 1: Clear outputs for zero data bits
+      // and wait until end of phase 1
       *portClear = phase1;
-      while (*timeValue < t1);
+      while (*timeValue < TICKS_PHASE_1);
+      
+      // Phase 2: Clear remaining outputs
       *portClear = phase2;
   
       // shift the bit mask
       bitMask >>= 1;
     }
-    
-    // next byte;
-    ++index;
-    
   }
-  while (*timeValue < period);    
+  
+  // Wait for final bit to complete
+  while (*timeValue < TICKS_PHASE_2);    
+  
+  // Stop the timer
   TC_Stop(TC1, 0);
 
+  // Interrupts, you can come back...
   interrupts();
-  endTime = micros(); // Save EOD time for latch on next call
+  
+  // Remember the current time so we can enforce minimum 
+  // latch period between refreshes
+  endTime = micros(); 
 }
 
-typedef struct 
+
+/////////////////////////////////////////////////////////////////////
+#define HALF_STRIP_JOIN 32 // represents a virtual "out of view" section at top of each strip
+#define LEFT_STRIP_MIN (PIX_LENGTH-HALF_STRIP_JOIN)
+#define LEFT_STRIP_MAX (-HALF_STRIP_JOIN)
+#define RIGHT_STRIP_MAX HALF_STRIP_JOIN
+#define RIGHT_STRIP_MIN (PIX_LENGTH+HALF_STRIP_JOIN)
+
+typedef struct
 {
-  byte x;
-  int  c;
-  byte i;
-  float y;
+  int x;         // which column (strip)
+  float y;         // row position (0 is at the top of the strip)
+  float intensity; // spark intensity, which ranges from bright white to dull red
   float dy;
-} BLOB;
-#define MAX_BLOB 100
-BLOB blobs[MAX_BLOB];
-void initBlobs()
+} SPARK;
+
+#define NUM_SPARKS 20
+SPARK sparks[NUM_SPARKS];
+void sparksInit()
 {
-  memset(blobs,0,sizeof(blobs));
-}
-void runBlobs()
-{
-  for(int i=0; i<MAX_BLOB; ++i)
+  for(int i=0; i<NUM_SPARKS; ++i)
   {
-    BLOB *p = &blobs[i];
-    if(p->y <=0)
+    sparks[i].x=0;
+    sparks[i].y=0;
+    sparks[i].intensity=0;
+    sparks[i].dy=0;
+  }
+}
+void sparksRender()
+{
+  for(int i=0; i<NUM_SPARKS; ++i)
+  {    
+    if(sparks[i].intensity>5)
     {
-      p->y = 149;
-      p->dy = 0.1;
-      p->x = i%3;
-      p->c = random(1000);
-      p->i = 20+random(235);
-    }
-    else if(!p->c)
-    {
-      p->y -= p->dy;
-      p->dy += 0.0005;
-//      if(p->i>0)
-//        p->i--;
-    }
-    else
-    {
-        --p->c;
+      SPARK *pSpark = &sparks[i];
+      byte *pCell = NULL;
+      if(pSpark->x < 3)
+      {
+        // LEFT SIDE
+        int y = pSpark->y - LEFT_STRIP_MIN;
+        if(y >= 0 && y < PIX_LENGTH)
+          pCell = &pixBuffer[pSpark->x][3*y];
+      }
+      else 
+      {
+        // RIGHT SIDE
+        int y = pSpark->y - LEFT_STRIP_MIN;
+        if(y >= 0 && y < PIX_LENGTH)
+          pCell = &pixBuffer[pSpark->x-3][3*(PIX_LENGTH - y)];
+      }
+      if(pCell)
+      {
+#define THR        50
+        pCell[1] = sparks[i].intensity;
+        if(sparks[i].intensity > THR)
+          pCell[0] = pCell[2] = THR + sparks[i].intensity/2;
+      }
     }
   }
 }
-
-void renderBlobs()
+void sparksExplode()
 {
-  for(int i=0; i<MAX_BLOB; ++i)
+  for(int i=0; i<NUM_SPARKS; ++i)
   {
-    BLOB *p = &blobs[i];
-    if(p->y >= 0 && p->y < 149 )
-      pixBuffer[p->x][1+(3*(int)p->y)] = p->i;
+    sparks[i].x=random(3);
+    sparks[i].y=LEFT_STRIP_MIN;
+    sparks[i].intensity=(float)random(150)+50;
+    sparks[i].dy=(float)random(300)/100;
   }
+  
 }
-/*
-
-            .     .
-               X
-            X     X
-               .
-            X     X
-               X
-*/
+void sparksRun()
+{
+  for(int i=0; i<NUM_SPARKS; ++i)
+  {
+    if(sparks[i].intensity)
+    {
+      sparks[i].y+=sparks[i].dy;
+      sparks[i].dy*=0.93;
+      sparks[i].intensity*=0.95;
+    }
+  }
+  
+}
 
 void puckRender(int y)
 {
@@ -226,18 +294,31 @@ void puckRender(int y)
 }
 
 void setup() {  
-  initBlobs();
+  pinMode(P_HEARTBEAT, OUTPUT);
+  digitalWrite(P_HEARTBEAT, LOW);
+  sparksInit();
   pixInit();
 }
 //int puckY=0;
 int q=0;
+byte c=0;
+unsigned long nextTick = 0;
 void loop() 
 {
+  unsigned long m = millis();
+  if(m > nextTick)
+  {
+    nextTick = m + 200;
+    digitalWrite(P_HEARTBEAT, c);
+    c=!c;
+    sparksExplode();
+  }
+  sparksRun();
+  
   //runBlobs();
   pixClear();
-  puckRender(q);
-  if(++q>=150) q=0;
-//  renderBlobs();
+  sparksRender();
   pixUpdate();
+  
 }
 
