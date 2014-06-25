411f17b3a061b90946260c947fa5edef08d07bae
Jason Hotchkiss <jason_hotchkiss@hotmail.com>
2014-06-25 09:44:32 +0100

    test midi

--- HammerPong/HammerPong.ino
+++ HammerPong/HammerPong.ino
@@ -12,6 +12,85 @@
 #define P_HEARTBEAT 13
 
 
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+// LOW LEVEL MIDI HANDLING
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+// state variables
+byte midiInRunningStatus;
+byte midiOutRunningStatus;
+byte midiNumParams;
+byte midiParams[2];
+char midiParamIndex;
+
+////////////////////////////////////////////////////////////////////////////////
+// MIDI INIT
+void midiSetup()
+{
+  // init the serial port
+  Serial.begin(31250);
+  Serial.flush();
+
+  midiInRunningStatus = 0;
+  midiOutRunningStatus = 0;
+  midiNumParams = 0;
+  midiParamIndex = 0;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// MIDI READ
+byte midiRead()
+{
+  // loop while we have incoming MIDI serial data
+  while(Serial.available())
+  {    
+    // fetch the next byte
+    byte ch = Serial.read();
+
+    if(!!(ch & 0x80))
+    {
+      midiParamIndex = 0;
+      midiInRunningStatus = ch; 
+      switch(ch & 0xF0)
+      {
+        case 0xD0: //  Channel Pressure  1  pressure  
+          midiNumParams = 1;
+          break;    
+        case 0x80: //  Note-off  2  key  velocity  
+        case 0x90: //  Note-on  2  key  veolcity  
+        case 0xA0: //  Aftertouch  2  key  touch  
+        case 0xB0: //  Continuous controller  2  controller #  controller value  
+        case 0xC0: //  Patch change  2  instrument #   
+        case 0xE0: //  Pitch bend  2  lsb (7 bits)  msb (7 bits)  
+        default:
+          midiNumParams = 2;
+          break;        
+      }
+    }    
+    else if(midiInRunningStatus)
+    {
+      // gathering parameters
+      midiParams[midiParamIndex++] = ch;
+      if(midiParamIndex >= midiNumParams)
+      {
+        midiParamIndex = 0;
+        return midiInRunningStatus;        
+      }
+    }
+  }
+  return 0;
+}
+
+
 
 /*
 
@@ -141,6 +220,7 @@ void setup() {
   digitalWrite(P_HEARTBEAT, LOW);
   //sparksInit();
 //  pixInit();
+  midiSetup();
   stripSetup();
   lightsSetup();
   digitsSetup();
@@ -195,6 +275,18 @@ void loop()
 //  pixClear();
 //  sparksRender();
 //  pixUpdate();
+  if(midiRead() == 0x90 && midiParams[1])
+  {
+    switch(midiParams[0])
+    {
+      case 60:
+        digitsSetBoth(0xff,0);
+        break;
+      case 61:
+        digitsSetBoth(0,0xff);
+        break;
+    }
+  }
   gameRun(ticks);
 //  stripRun(0);
 }
